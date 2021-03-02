be1cb17b49b702989ee02150abcd54e7e66c4b26
Rafael-Mendes-de-Barros <70581646+Rafael-Mendes-de-Barros@users.noreply.github.com>
2021-03-02 08:12:54 -0300

    Create prog01.py

---
+++ LTP2-2021-1/aula2/prog01.py
@@ -0,0 +1,15 @@
+idade1 = 32
+idade2 = 31
+idade3 = 19
+idade4 = 20
+idade5 = 19
+idade6 = 24
+idade7 = 38
+
+print(f"Idade informada:{idade1}")
+print(f"Idade informada:{idade2}")
+print(f"Idade informada:{idade3}")
+print(f"Idade informada:{idade4}")
+print(f"Idade informada:{idade5}")
+print(f"Idade informada:{idade6}")
+print(f"Idade informada:{idade7}")
